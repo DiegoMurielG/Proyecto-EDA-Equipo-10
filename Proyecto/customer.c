a9d2157a63773bdae284937f768e48ccae9183a2
Diego Muriel G <65355590+DiegoMurielG@users.noreply.github.com>
2022-05-16 13:03:35 -0500

    Add files via upload
    Updated Names and includes

---
+++ Proyecto/customer.c
@@ -0,0 +1,439 @@
+/*Copyright (C) 
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * 2022 - EDA 1 - |	Team 10											|
+ * 				  |	Casanova Torres Gerardo Aldahir (Equipo 10) 	|
+ *				  |	Herrera Romero Juan Pablo						|
+ *				  |	Muriel Gonz�lez Diego							|
+ *				  |	Olvera Negrete Elliot Eusebio					|
+ */
+
+
+#include "customer_database_INC.h"
+
+static Node* new_node( int x )
+{
+   Node* n = (Node*) malloc( sizeof( Node ) );
+   if( n != NULL ){
+      n->datos = x;
+      n->next = NULL;
+      n->prev = NULL;
+   }
+
+   return n;
+}
+
+// Función auxiliar.
+//
+// Elimina el nodo apuntado por |pos|. Esta función es utilizada
+// por DLL_Find_if()
+//
+// Devuelve el nodo a la derecha del nodo recién eliminado.
+static Node* erase( DLL* list, Node* pos )
+{
+	return NULL;
+}
+
+/**
+ * @brief Crea una lista doblemente enlazada
+ *
+ * @return Una referencia a la nueva lista
+ * @post Una lista existente en el heap
+ */
+DLL* DLL_New()
+{
+	DLL* list = ( DLL* )malloc( sizeof(DLL) );
+	if( list ){
+		list->first = list->last = list->cursor = NULL;
+		list->len = 0;
+	}
+	
+	return list;
+}
+
+/**
+ * @brief Destruye una lista.
+ *
+ * @param this Una lista.
+ */
+void DLL_Delete( DLL** this )
+{
+	assert( *this );
+	
+	DLL_MakeEmpty( *this );
+	free( *this );
+	*this = NULL;
+}
+
+/**
+ * @brief Inserta un elemento a la derecha del cursor
+ *
+ * @param lista Una referencia a la lista de trabajo
+ * @param item El elemento a insertar
+ *
+ * @post Coloca al cursor en el nuevo nodo
+ *
+ */
+void DLL_Insert( DLL* this, int item )
+{
+	assert( this );
+	assert( this->cursor );
+	
+	if( DLL_IsEmpty( this ) ){
+		Node* n = new_node( item );
+		this->first = this->last = this->cursor = n;
+		++this->len;
+	}else if( this->cursor == this->last ){
+		DLL_Push_back( this, item );
+	}else{
+		Node* n = new_node( item );
+		Node* right = this->cursor->next;
+		n->next = right;
+		this->cursor->next = n;
+		right->prev = n;
+		n->prev = this->cursor;
+		this->cursor = n;
+		++this->len;
+	}
+}
+
+/**
+ * @brief Inserta un elemento en el frente de la lista.
+ *
+ * @param this Una lista.
+ * @param item El elemento a insertar
+ */
+void DLL_Push_front( DLL* this, int item )
+{
+	Node* n = new_node( item );
+   	assert( n );
+   	
+	if( DLL_IsEmpty( this ) ){
+		this->first = this->last = this->cursor = n;
+		
+	}else{
+		Node* right = this->first;
+		this->first = n;
+		this->first->next = right;
+		right->prev = this->first;
+		
+	}
+	++this->len;
+}
+
+/**
+ * @brief Inserta un elemento en el fondo de la lista.
+ *
+ * @param this Una lista.
+ * @param item El elemento a insertar
+ */
+void DLL_Push_back( DLL* this, int item )
+{
+   Node* n = new_node( item );
+   assert( n );
+
+   if( this->first != NULL ){
+
+      this->last->next = n;
+      n->prev = this->last;
+      this->last = n;
+   } else{
+      this->first = this->last = this->cursor = n;
+   }
+   ++this->len;
+}
+
+/**
+ * @brief Elimina el elemento al frente de la lista.
+ *
+ * @param this Referencia a una lista.
+ *
+ * @post El cursor se mantiene en la posición en la que estaba cuando entró la
+ * función.
+ */
+void DLL_Pop_front( DLL* this )
+{
+	assert( this );
+	assert( this->len );//Len es mayor a 0; 0 es NULL
+   // ERR: no se puede borrar nada de una lista vacía
+	
+	if( this->first == this->last ){//Solo 1 elemento
+		free( this->first );
+		this->first = this->last = this->cursor = NULL;
+	}else{ //Mas de 1 elemento en la lista
+		Node* right = this->first->next;
+		free( this->first );
+		this->first = right;
+		
+		this->first->prev = NULL;
+	}
+	--this->len;
+}
+
+/**
+ * @brief Elimina el elemento al fondo de la lista.
+ *
+ * @param this Referencia a una lista.
+ *
+ * @post El cursor se mantiene en la posición en la que estaba cuando entró la
+ * función.
+ */
+void DLL_Pop_back( DLL* this )
+{
+   assert( this->len );
+   // ERR: no se puede borrar nada de una lista vacía
+
+   if( this->last != this->first ) // también funciona: if( this->len > 1 ){...}
+   { 
+      Node* x = this->last->prev;
+      free( this->last );
+      x->next = NULL;
+      this->last = x;
+      
+   } else{
+      free( this->last );
+      this->first = this->last = this->cursor = NULL;
+   }
+   --this->len;
+}
+
+/**
+ * @brief Devuelve una copia del valor apuntado por el cursor
+ *
+ * @param this Una lista
+ *
+ * @return El valor apuntado por el cursor
+ */
+int DLL_Cursor_Get( DLL* this )
+{
+}
+
+/**
+ * @brief Coloca al cursor al inicio de la lista.
+ *
+ * @param this Una referencia a la lista de trabajo
+ */
+void DLL_Cursor_front( DLL* this )
+{
+   this->cursor = this->first;
+}
+
+/**
+ * @brief Coloca al cursor al final de la lista.
+ *
+ * @param this Una referencia a la lista de trabajo
+ */
+void DLL_Cursor_back( DLL* this )
+{
+}
+
+/**
+ * @brief Mueve al cursor al siguiente elemento a la derecha.
+ *
+ * @param this Una lista.
+ */
+void DLL_Cursor_next( DLL* this )
+{
+}
+
+/**
+ * @brief Mueve al cursor al siguiente elemento a la izquierda.
+ *
+ * @param this Una lista.
+ */
+void DLL_Cursor_prev( DLL* this )
+{
+   assert( this->cursor != NULL );
+
+   Node* left = this->cursor->prev;
+   this->cursor = left;
+}
+
+/**
+ * @brief Indica si la lista está vacía.
+ *
+ * @param this Referencia a una lista.
+ *
+ * @return true si la lista está vacía; false en caso contrario.
+ */
+bool DLL_IsEmpty( DLL* this )
+{
+   return this->first == NULL;
+}
+
+/**
+ * @brief Devuelve el número actual de elementos en la lista.
+ *
+ * @param this Una lista.
+ *
+ * @return Devuelve el número actual de elementos en la lista.
+ */
+size_t DLL_Len( DLL* this )
+{
+}
+
+/**
+ * @brief Elimina todos los elementos de la lista sin eliminar la lista.
+ *
+ * @param this Una lista
+ */
+void DLL_MakeEmpty( DLL* this )
+{
+	DLL_Cursor_front( this );
+	
+	while( !DLL_IsEmpty( this ) ){// DLL_IsEmpty( this ) == false
+		DLL_Erase( this );
+	}
+	assert( this->len == 0 );
+	//Assert si len no se coloo en 0 al borrar toda la lista
+	
+	
+	//Estado seguro de Reset de la lista
+	this->first = this->last = this->cursor = NULL;
+	this->len = 0; // Lo ponemos porque se pueden desactivar los assert
+	
+}
+
+/**
+ * @brief Devuelve una copia del elemento en el frente de la lista
+ *
+ * @param this Una lista
+ *
+ * @return La copia del elemento en el frente de la lista
+ *
+ * @pre La lista NO debe estar vacía
+ */
+int DLL_Front( DLL* this )
+{
+   assert( this->first != NULL );
+   // ERR: no se puede leer de una lista vacía
+
+   return this->first->datos;
+}
+
+/**
+ * @brief Devuelve una copia del elemento en la parte trasera de la lista.
+ *
+ * @param this Una lista.
+ *
+ * @return La copia del elemento en la parte trasera de la lista.
+ *
+ * @pre La lista NO debe estar vacía
+ */
+int DLL_Back( DLL* this )
+{
+	assert( this );
+	assert( this->len );
+	//Assert en caso de que la lista este vacIa
+	
+	return this->last->datos;
+}
+
+/**
+ * @brief Elimina el elemento apuntado por el cursor.
+ *
+ * @param this Una lista.
+ *
+ * @post El cursor se mueve a la derecha.
+ *
+ */
+void DLL_Erase( DLL* this )
+{
+	if( this->cursor == this->first ){
+		DLL_Pop_front( this );
+		this->cursor == this->first;
+	}else if( this->cursor == this->last ){
+		DLL_Pop_back( this );
+		this->cursor == this->last;
+	}else{
+		Node* left = this->cursor->prev;
+		Node* right = this->cursor->next;
+		
+		free( this->cursor );
+		
+		left->next = right;
+		right->prev = left;
+		--this->len;
+		this->cursor = right;
+	}
+}
+
+/**
+ * @brief Elimina el primer nodo que coincida con la llave.
+ *
+ * @param this Una lista
+ * @param key  Valor buscado
+ *
+ */
+void DLL_Remove( DLL* this, int key )
+{
+}
+
+/**
+ * @brief Busca un valor en la lista. Si lo encuentra coloca ahí al cursor.
+ *
+ * @param this Una lista.
+ * @param key El valor que se está buscando.
+ *
+ * @post Si se encontró una coincidencia coloca al cursor en ese nodo; en caso contrario el cursor es NULL.
+ *
+ * @code
+ * if( DLL_Find( list, x ) )
+ * {
+ *    y = DLL_Cursor_Get( list );
+ *
+ *    // ...
+ * }
+ * @endcode
+ */
+bool DLL_Find( DLL* this, int key )
+{
+   for( this->cursor = this->first; 
+        this->cursor != NULL; 
+        this->cursor = this->cursor->next )
+   {
+      if( this->cursor->datos == key )
+      {
+         return true;
+      }
+   }
+   return false;
+}
+
+/**
+ * @brief Imprime la estructura de la lista. Es para uso de depuración, no es de uso general.
+ *
+ * @param this Una lista.
+ */
+void DLL_PrintStructure( DLL* this )
+{
+   if( DLL_IsEmpty( this ) )
+   {
+      fprintf( stderr, "Empty list. Nothing to show.\n" );
+   } 
+   else
+   {
+      fprintf( stderr, "\nNil->" );
+
+      // usando un for en lugar de while (mi preferido):
+      for( Node* it = this->first; it != NULL; it = it->next )
+      {
+         fprintf( stderr, "(%d)->", it->datos );
+      } 
+      fprintf( stderr, "Nil\n" );
+   }
+}
+
---
+++ Proyecto/warehouse_INC.h
@@ -0,0 +1,96 @@
+/*Copyright (C) 
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * 2022 - EDA 1 - |	Team 10											|
+ * 				  |	Casanova Torres Gerardo Aldahir (Equipo 10) 	|
+ *				  |	Herrera Romero Juan Pablo						|
+ *				  |	Muriel González Diego							|
+ *				  |	Olvera Negrete Elliot Eusebio					|
+ */
+
+
+#ifndef  warehouse_INC
+#define  warehouse_INC
+#define MAX_CHAR 30
+
+#include <stdlib.h>
+// para malloc and friends y size_t
+
+#include <stdbool.h>
+// para bool and friends
+
+#include <assert.h>
+// para las aserciones: assert()
+
+#include <stdio.h>
+// para fprintf()
+
+typedef struct product_node
+{
+	char product_name[ MAX_CHAR ];
+	int product_amount;
+	int product_id;
+	
+
+	struct product_node* next;
+	struct product_node* prev;
+} product_node;
+
+typedef struct
+{
+	product_node* first;
+	product_node* last;
+	product_node* cursor;
+	size_t warehouse_len;
+} warehouse_DLL;
+
+DLL* DLL_New();
+void DLL_Delete( DLL** this );
+
+int DLL_Front( DLL* this );
+int DLL_Back( DLL* this );
+
+void DLL_Push_front( DLL* this, int item );
+void DLL_Push_back( DLL* this, int item );
+void DLL_Insert( DLL* this, int item );
+
+void DLL_Pop_front( DLL* this );
+void DLL_Pop_back( DLL* this );
+
+void DLL_Erase( DLL* this );
+void DLL_Remove( DLL* this, int key );
+
+void DLL_MakeEmpty( DLL* this );
+
+bool DLL_Find( DLL* this, int key );
+
+int  DLL_Cursor_Get( DLL* this );
+void DLL_Cursor_front( DLL* this );
+void DLL_Cursor_back( DLL* this );
+void DLL_Cursor_next( DLL* this );
+void DLL_Cursor_prev( DLL* this );
+bool DLL_Cursor_end( DLL* this );
+
+bool DLL_IsEmpty( DLL* this );
+size_t DLL_Len( DLL* this );
+
+
+void DLL_PrintStructure( DLL* this );
+
+
+
+
+#endif   /* ----- #ifndef warehouse_INC  ----- */
