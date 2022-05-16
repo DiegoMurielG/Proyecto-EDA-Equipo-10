fcdeb8c05de306194c13687ac9037a036226ae4e
Diego Muriel G <65355590+DiegoMurielG@users.noreply.github.com>
2022-05-16 11:52:42 -0500

    Add files via upload
    Separated the structures in their respective modules with headers.

---
+++ company_INC.h
@@ -0,0 +1,93 @@
+/*Copyright (C) 
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * 2022 - EDA 1 - |	Team 10											|
+ * 				  |	Casanova Torres Gerardo Aldahir (Equipo 10) 	|
+ *				  |	Herrera Romero Juan Pablo						|
+ *				  |	Muriel González Diego							|
+ *				  |	Olvera Negrete Elliot Eusebio					|
+ */
+
+
+#ifndef  company_INC
+#define  company_INC
+#define MAX_CHAR 30
+
+#include <stdlib.h>
+// para malloc and friends y size_t
+
+#include <stdbool.h>
+// para bool and friends
+
+#include <assert.h>
+// para las aserciones: assert()
+
+#include <stdio.h>
+// para fprintf()
+
+#include "warehouse_INC.h"
+#include "clients_database_INC.h"
+
+
+typedef struct{
+	char name[ MAX_CHAR ];
+	char members[ MAX_CHAR*9 ];
+	int operations_year;
+	warehouse_DLL* warehouse;
+	customer_database_DLL* customer_database;
+	
+} Company;
+
+DLL* DLL_New();
+void DLL_Delete( DLL** this );
+
+int DLL_Front( DLL* this );
+int DLL_Back( DLL* this );
+
+void DLL_Push_front( DLL* this, int item );
+void DLL_Push_back( DLL* this, int item );
+void DLL_Insert( DLL* this, int item );
+
+void DLL_Pop_front( DLL* this );
+void DLL_Pop_back( DLL* this );
+
+void DLL_Erase( DLL* this );
+void DLL_Remove( DLL* this, int key );
+
+void DLL_MakeEmpty( DLL* this );
+
+bool DLL_Find( DLL* this, int key );
+
+int  DLL_Cursor_Get( DLL* this );
+void DLL_Cursor_front( DLL* this );
+void DLL_Cursor_back( DLL* this );
+void DLL_Cursor_next( DLL* this );
+void DLL_Cursor_prev( DLL* this );
+bool DLL_Cursor_end( DLL* this );
+
+bool DLL_IsEmpty( DLL* this );
+size_t DLL_Len( DLL* this );
+
+
+void DLL_PrintStructure( DLL* this );
+
+
+
+
+
+
+
+#endif   /* ----- #ifndef company_INC  ----- */
